313c4ad97f197ffb4045cf6e126e75874ec3e133
OniTakai <steven.miller.it@gmail.com>
2017-08-25 12:25:15 -0400

    Just need results to work

--- app.rb
+++ app.rb
@@ -16,3 +16,22 @@ end
 get '/confirm' do
     erb :confirm, locals: {my_size: session[:my_size],my_meat: session[:my_meat],my_veg: session[:my_veg],my_cheese: session[:my_cheese]}
 end
+
+post 'confirm' do
+    session[:my_size] = params[:my_size]
+    session[:my_meat] = params[:my_meat]
+    session[:my_veg] = params[:my_veg]
+    session[:my_cheese] = params[:my_cheese]
+    redirect '/results'
+end 
+
+get '/results' do 
+    erb :results, locals: {my_size_confirm: session[my_size_confirm], my_meat_confirm: session[my_meat_confirm],my_veg_confirm: session[my_veg_confirm], my_cheee_confirm: session[my_cheese_confirm]}
+end 
+
+post '/results' do
+    session[:my_size_confirm] = params[:my_size_confirm]
+    session[:my_meat_confirm] = params[:my_meat_confirm]
+    session[:my_veg_confirm] = params[:my_veg_confirm]
+    session[:my_cheese_confirm] = params[:my_cheese_confirm]
+end
