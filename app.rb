fd77a5d578d0da5799d0c8df737b2e0ac966d2c0
OniTakai <steven.miller.it@gmail.com>
2017-08-24 21:51:44 -0400

    Got pictures tranfering earlier, still working on results

--- app.rb
+++ app.rb
@@ -4,3 +4,15 @@ enable :sessions
 get '/' do
     erb :index
 end 
+
+post '/toppings' do
+    session[:my_size] = params[:my_size]
+    session[:my_meat] = params[:my_meat]
+    session[:my_veg] = params[:my_veg]
+    session[:my_cheese] = params[:my_cheese]
+    redirect '/confirm'
+end
+
+get '/confirm' do
+    erb :confirm, locals: {my_size: session[:my_size],my_meat: session[:my_meat],my_veg: session[:my_veg],my_cheese: session[:my_cheese]}
+end
